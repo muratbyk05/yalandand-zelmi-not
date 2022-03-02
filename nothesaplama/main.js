8b33a6ca61a58b64b8369e054069b0a6c753961a
muratbyk05 <95565340+muratbyk05@users.noreply.github.com>
2022-03-02 11:11:51 +0300

    Add files via upload

---
+++ nothesaplama/main.js
@@ -0,0 +1,12 @@
+function hesapla(){
+
+    
+    not1=document.getElementById("birnot").value
+       not2=document.getElementById("ikinot").value
+       per=document.getElementById("perf").value
+    
+     
+     let ortalama=(not1*1+not2*1+per*1)/3
+
+     window.alert(ortalama)
+}
